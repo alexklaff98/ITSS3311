7833e057ea1dfee0a135200d3444b7f59b411f90
alexklaff98 <64282631+alexklaff98@users.noreply.github.com>
2020-04-24 14:35:47 -0500

    Add files via upload

--- StrictlyIdentical.java
+++ StrictlyIdentical.java
@@ -1,21 +1,45 @@
+
 import java.util.Scanner;
 public class StrictlyIdentical {
 
+	
+	//sample run and github
+	/*
+
+
+	https://github.com/alexklaff98/ITSS3311.git
+
+
+
+	Enter first list of 5 integers: 
+	1 2 3 4 5
+	Enter second list of 5 integers: 
+	1 2 3 4 5
+	Two lists are strictly identical
+	*/
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         
+         //scanner for input
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter first list of 5 integers: ");
-		
+		//declares first list with size 5
 		int[] list1= new int[5];
 		for(int i=0; i< list1.length; i++) list1[i]=input.nextInt();
 		
-		System.out.println("Enter second list of 5 integers: ");
 		
+		//prompts user to input the second list
+		System.out.println("Enter second list of 5 integers: ");
+		//declares second list with size 5
 		int[] list2= new int[5];
 		for (int i=0; i<list2.length;i++) list2[i]=input.nextInt();
 		
-		
+		//calls on equal method to determine if the lists are identical
 		if(equals(list1, list2)) {
 			System.out.println("Two lists are strictly identical");
 		} else {
@@ -27,9 +51,9 @@ public class StrictlyIdentical {
 		
 	}
 
-	public static boolean equals(int[] list1, int[] list2)
+	public static boolean equals(int[] list1, int[] list2)//equals method
 	{
-		if(list1.length != list2.length)return false;
+		if(list1.length != list2.length)return false; //comparing length
 		
 		for(int i=0; i<list1.length; i++) {
 			if(list1[i] != list2[i]) return false;
@@ -40,3 +64,18 @@ public class StrictlyIdentical {
 	
 	
 }
+
+//sample run and github
+/*
+
+
+https://github.com/alexklaff98/ITSS3311.git
+
+
+
+Enter first list of 5 integers: 
+1 2 3 4 5
+Enter second list of 5 integers: 
+1 2 3 4 5
+Two lists are strictly identical
+*/
